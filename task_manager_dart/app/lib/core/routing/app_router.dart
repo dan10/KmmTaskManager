58e86f4be2f861a18acf540ed4cd3564bbbac816
Daniel Oliveira <danielsantosoliveira753@gmail.com>
2025-10-23 19:04:50 -0300

    feat: Add authentication redirect logic to app router

--- task_manager_dart/app/lib/core/routing/app_router.dart
+++ task_manager_dart/app/lib/core/routing/app_router.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import '../../features/auth/routing/auth_routes.dart';
 import '../../features/projects/routing/project_routes.dart';
 import '../../features/tasks/routing/task_routes.dart';
 import '../../presentation/views/main/main_view.dart';
+import '../../data/sources/local/secure_storage.dart';
 
 // It's good practice to have your route paths as constants
 class AppRoutes {
@@ -29,6 +31,7 @@ final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>();
 final GoRouter appRouter = GoRouter(
   navigatorKey: _rootNavigatorKey,
   initialLocation: AppRoutes.login, // The first route to show
+  redirect: _authRedirect,
   routes: [
     GoRoute(
       path: AppRoutes.home,
@@ -47,3 +50,22 @@ final GoRouter appRouter = GoRouter(
     body: Center(child: Text('Page not found: ${state.error}')),
   ),
 );
+
+Future<String?> _authRedirect(BuildContext context, GoRouterState state) async {
+  final secureStorage = Provider.of<SecureStorage>(context, listen: false);
+  final token = await secureStorage.getToken();
+  final loggedIn = token != null && token.isNotEmpty;
+
+  final isAuthRoute = state.matchedLocation == AppRoutes.login ||
+      state.matchedLocation == AppRoutes.register;
+
+  if (!loggedIn && !isAuthRoute) {
+    return AppRoutes.login;
+  }
+
+  if (loggedIn && isAuthRoute) {
+    return AppRoutes.home;
+  }
+
+  return null;
+}
